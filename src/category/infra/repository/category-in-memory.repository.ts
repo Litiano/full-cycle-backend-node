5eee2e2c349dd1ef86ba063b9cb1d5b06152e01f
Litiano Moura <litiano.moura@yahoo.com.br>
2022-11-20 19:26:49 -0300

    Category In Memory Repository.

--- src/category/infra/repository/category-in-memory.repository.ts
+++ src/category/infra/repository/category-in-memory.repository.ts
@@ -1,11 +1,14 @@
 import { Category } from '../../domain/entities/category';
 import InMemorySearchableRepository from '../../../@seedwork/domain/repository/in-memory-searchable.repository';
 import CategoryRepository from '../../domain/repository/category-repository-interface';
+import { SortDirection } from '../../../@seedwork/domain/repository/search-params';
 
 export default class CategoryInMemoryRepository
   extends InMemorySearchableRepository<Category>
   implements CategoryRepository.RepositoryInterface
 {
+  sortableFields: string[] = ['name', 'created_at'];
+
   protected async applyFilter(items: Category[], filter: string | null): Promise<Category[]> {
     if (!filter) {
       return items;
@@ -14,5 +17,12 @@ export default class CategoryInMemoryRepository
     return items.filter(i => {
       return i.props.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase());
     });
+  }
+
+  protected async applySort(items: Category[], sort: string | null, sort_dir: SortDirection | null): Promise<Category[]> {
+    if (sort) {
+      return super.applySort(items, sort, sort_dir)
+    }
+    return super.applySort(items, 'created_at', 'desc')
   }
 }
