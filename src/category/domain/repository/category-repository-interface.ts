d92b37e7891b58b9a64c9a8821832196b8bcc249
Litiano Moura <litiano.moura@yahoo.com.br>
2022-11-20 19:05:49 -0300

    Category repository.

--- src/category/domain/repository/category-repository-interface.ts
+++ src/category/domain/repository/category-repository-interface.ts
@@ -1,6 +1,17 @@
 import { Category } from '../entities/category';
 import { SearchableRepositoryInterface } from '../../../@seedwork/domain/repository/searchable-repository-interface';
+import DefaultSearchParams from '../../../@seedwork/domain/repository/search-params';
+import DefaultSearchResult from '../../../@seedwork/domain/repository/search-result';
 
-export default interface CategoryRepositoryInterface extends SearchableRepositoryInterface<Category, any, any> {
+export namespace CategoryRepository {
+  export type Filter = string;
 
+  export class SearchParams extends DefaultSearchParams<Filter> {}
+
+  export class SearchResult extends DefaultSearchResult<Category, Filter> {}
+
+  export interface RepositoryInterface
+    extends SearchableRepositoryInterface<Category, Filter, SearchParams, SearchResult> {}
 }
+
+export default CategoryRepository;
--- src/category/infra/repository/category-in-memory.repository.ts
+++ src/category/infra/repository/category-in-memory.repository.ts
@@ -1,9 +1,18 @@
-import InMemoryRepository from '../../../@seedwork/domain/repository/in-memory.repository';
 import { Category } from '../../domain/entities/category';
-import CategoryRepositoryInterface from '../../domain/repository/category-repository-interface';
+import InMemorySearchableRepository from '../../../@seedwork/domain/repository/in-memory-searchable.repository';
+import CategoryRepository from '../../domain/repository/category-repository-interface';
 
-export default class CategoryInMemoryRepository extends InMemoryRepository<Category> implements CategoryRepositoryInterface {
-    search(_props: any): Promise<any> {
-        return Promise.resolve(undefined);
+export default class CategoryInMemoryRepository
+  extends InMemorySearchableRepository<Category>
+  implements CategoryRepository.RepositoryInterface
+{
+  protected async applyFilter(items: Category[], filter: string | null): Promise<Category[]> {
+    if (!filter) {
+      return items;
     }
+
+    return items.filter(i => {
+      return i.props.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase());
+    });
+  }
 }
